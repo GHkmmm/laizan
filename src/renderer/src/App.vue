a87548e4c63def2a6c0e2ec8a7727bf326fe6db6
ira <hgmkk2299@icloud.com>
2025-09-25 18:01:12 +0800

    feat: project init

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const startTask = (): void => {
+  window.electron.ipcRenderer.send('startTask')
+}
+</script>
+
+<template>
+  <div>
+    <button class="start-button" @click="startTask">开始任务</button>
+  </div>
+</template>
